bcc817bab628bbe9079f352177027579b3f43e73
CarlosLaraFP <info@carloslaraai.com>
2023-07-30 17:22:31 -0700

    Single threaded bottleneck

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration
 };
 
 type Result = anyhow::Result<()>;
@@ -17,12 +19,13 @@ fn main() -> Result {
     for stream in listener.incoming() {
         handle_connection(stream?)?;
     }
+
+    Ok(())
+
     /*
         When stream goes out of scope and is dropped at the end of the loop,
         the connection is closed as part of the drop implementation.
      */
-
-    Ok(())
 }
 
 fn handle_connection(mut stream: TcpStream) -> Result {
@@ -30,16 +33,22 @@ fn handle_connection(mut stream: TcpStream) -> Result {
     // first line is always of the form: "GET / HTTP/1.1"
     let request_line = reader.lines().next().unwrap()?;
 
-    let (status, contents) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", fs::read_to_string("hello.html")?)
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", fs::read_to_string("404.html")?)
+    let (status, file) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
+    let contents = fs::read_to_string(file)?;
     let length = contents.len(); // ensures a valid HTTP response
     let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes())?;
+
+    Ok(())
 
     /*
     let http_request: Vec<_> = reader
@@ -48,5 +57,4 @@ fn handle_connection(mut stream: TcpStream) -> Result {
         .take_while(|line| !line.is_empty())
         .collect();
     */
-    Ok(())
 }
